be8b43bbdfe2728c3d8c01aad07ec659d7e8c540
Chris Steege <steegecas@gmail.com>
2022-07-22 14:08:31 -0400

    Test

--- src/action.js
+++ src/action.js
@@ -1,39 +1,16 @@
 const fs = require('fs')
 const core = require('@actions/core')
+const github = require('@actions/github')
 
+
+console.log(github.context.payload.push)
 const ABSOLUTE_PATH = core.getInput('ABSOLUTE_PATH')
 
 const DEPLOYMENT_CONFIGURATIONS_JSON = require(ABSOLUTE_PATH + "/deployment/deployment.json");
 const DEPLOYMENT_CONFIGURATIONS = JSON.parse(JSON.stringify(DEPLOYMENT_CONFIGURATIONS_JSON));
 
 async function deploySubgraphs(ABSOLUTE_PATH) {
-    for (subgraph in DEPLOYMENT_CONFIGURATIONS["subgraphs"]) {
-        for (protocol in DEPLOYMENT_CONFIGURATIONS["subgraphs"][subgraph]) {
-            for (network in DEPLOYMENT_CONFIGURATIONS["subgraphs"][subgraph][
-                protocol
-            ]) {
-                if (
-                "deploy-on-merge" in
-                DEPLOYMENT_CONFIGURATIONS["subgraphs"][subgraph][protocol][network]
-                ) {
-                DEPLOYMENT_CONFIGURATIONS["subgraphs"][subgraph][protocol][network][
-                    "deploy-on-merge"
-                ] = false;
-                }
-            }
-        }
-    }
     
-    fs.writeFile(
-        ABSOLUTE_PATH + "/deployment/deployment.json",
-        JSON.stringify(DEPLOYMENT_CONFIGURATIONS, null, "\t"),
-        (err) => {
-        if (err) {
-            throw err;
-        }
-        console.log("JSON data is saved.");
-        }
-    );
 }
 
 deploySubgraphs(ABSOLUTE_PATH);
